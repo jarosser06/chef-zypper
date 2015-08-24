a79876ee0c61ec7375ff19e64451a110a77139d1
Jim Rosser <jarosser06@gmail.com>
2015-08-24 10:51:54 -0500

    Implemented key import
    If a key is provided then the zypper_repo provider will
    attempt to import it.

--- test/cookbooks/zypper-test/recipes/default.rb
+++ test/cookbooks/zypper-test/recipes/default.rb
@@ -1,9 +1,16 @@
 include_recipe 'zypper'
 
-zypper_repo 'SLES11SP3-x64 DVD1 Online' do
+zypper_repo 'remove_dvd_repo' do
   action :remove
+  repo_name 'SLES11SP3-x64 DVD1 Online'
 end
 
-zypper_repo 'SLES11SP3-x64 DVD1 Online' do
+zypper_repo 'add_dvd_repo' do
+  repo_name 'SLES11SP3-x64 DVD1 Online'
   uri 'http://demeter.uni-regensburg.de/SLES11SP3-x64/DVD1/'
+end
+
+zypper_repo 'jenkins' do
+  key 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
+  uri 'http://pkg.jenkins-ci.org/opensuse/'
 end
