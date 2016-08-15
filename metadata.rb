41151282f8565068a0535342ec19a5027abaf394
Tim Smith <tsmith@chef.io>
2016-08-14 20:56:32 -0700

    Update metadata with new fields and more platforms
    Add opensuse and opensuseleap, which aren't the same as suse on the
    supermarket
    
    Add the new metadata to resolve foodcritic warnings
    
    Add a more accurate description
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,14 @@ name             'zypper'
 maintainer       'Jim Rosser'
 maintainer_email 'jarosser06@gmail.com'
 license          'MIT'
-description      'Installs/Configures zypper'
+description      'Provides resources for managing zypper repositories'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.1'
 supports         'suse'
+supports         'opensuse'
+supports         'opensuseleap'
+
+source_url 'https://github.com/jarosser06/chef-zypper' if respond_to?(:source_url)
+issues_url 'https://github.com/jarosser06/chef-zypper/issues' if respond_to?(:issues_url)
+
+chef_version '>= 11' if respond_to?(:chef_version)
