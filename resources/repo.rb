dd29fba3c33811e065b48ae169415adaa2384df3
Luca Boccassi <lboccass@brocade.com>
2016-08-25 16:12:17 +0100

    Add repository priority attribute
    A "priority" integer attribute is added for the "add" action.
    If the add is successful, it will run:
      zypper mr -p <priority> <repo_name>

--- resources/repo.rb
+++ resources/repo.rb
@@ -5,3 +5,4 @@ attribute :repo_name, kind_of: String, name_attribute: true
 attribute :autorefresh, kind_of: [TrueClass, FalseClass]
 attribute :uri, kind_of: String
 attribute :key, kind_of: String, default: nil
+attribute :priority, kind_of: Integer, default: nil
--- test/cookbooks/zypper-test/recipes/default.rb
+++ test/cookbooks/zypper-test/recipes/default.rb
@@ -14,3 +14,9 @@ zypper_repo 'jenkins' do
   key 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
   uri 'http://pkg.jenkins-ci.org/opensuse/'
 end
+
+zypper_repo 'add_dvd_repo_high_priority' do
+  repo_name 'SLES11SP3-x64 DVD2 Online'
+  uri 'http://demeter.uni-regensburg.de/SLES11SP3-x64/DVD2/'
+  priority 10
+end
